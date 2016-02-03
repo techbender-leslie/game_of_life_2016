9bf9783083ebde4a444159ffb3e61a7559c13db4
techbender-leslie <techbenderleslie@gmail.com>
2016-02-02 20:35:17 -0800

    I can console log the index and value of each spot and console log the index of each cell, but not together yet

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// $(); //document ready in jquery
+
 
 // var spots= [1,2,3,4,5,6,12,18,24,23,22,21,20,26,32,33,34,35,36,42,48,54,53,52,51,50,56,62,68,69,70,71,77,83,82,81,80,79,85,91,92,93,94,95,96];
 
@@ -6,25 +6,28 @@
 //   var cell= $("#" + id);
 //   cell.attr("data-spot-id", i+1); 
 //   });
-var spots= [1,2,3,4,5,6,12,18,24,23,22,21,20,26,32,33,34,35,36,42,48,54,53,52,51,50,56,62,68,69,70,71,77,83,82,81,80,79,85,91,92,93,94,95,96];
+
+
+
 
 $(function() {
-  spots.forEach (function (id,i) {
-    var $spot= $("#" + id);
-    $spot.attr("data-spot-id", i+1);
+  $( window ).load(function() {
+        console.log( "window loaded" );
+
+  var spots= [1,2,3,4,5,6,12,18,24,23,22,21,20,26,32,33,34,35,36,42,48,54,53,52,51,50,56,62,68,69,70,71,77,83,82,81,80,79,85,91,92,93,94,95,96]; 
+
+  $.each(spots, function (index, value) {
+  console.log(index + ":" + value);
   });
 
- var $boxes = $(".cell");
+  $('.cell').each(function (index,value) {
+  console.log(index);
+  });
 
+
+ var $boxes = $(".cell");
   $boxes.html("Hello, little boxes");
   $boxes.css("background-color","white");
 
+ });
 });
-
-
-
-
-
-
-
-
