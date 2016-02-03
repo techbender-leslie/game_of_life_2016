f28071c08c65b9b776842318a519dca9bf161046
techbender-leslie <techbenderleslie@gmail.com>
2016-02-03 01:17:28 -0800

    small changes and notes added

--- game.js
+++ game.js
@@ -1,42 +1,32 @@
-// 44 active spots on the board so far
+// 45 active spots on the board so far
 
-// var spots= [1,2,3,4,5,6,12,18,24,23,22,21,20,26,32,33,34,35,36,42,48,54,53,52,51,50,56,62,68,69,70,71,77,83,82,81,80,79,85,91,92,93,94,95,96];
-
+var gameSpots= [1,2,3,4,5,6,12,18,24,23,22,21,20,26,32,33,34,35,36,42,48,54,53,52,51,50,56,62,68,69,70,71,77,83,82,81,80,79,85,91,92,93,94,95,96]; 
 
 $(function() {
   $( window ).load(function() {
         console.log( "window loaded" );
 
-  var gameSpots= [1,2,3,4,5,6,12,18,24,23,22,21,20,26,32,33,34,35,36,42,48,54,53,52,51,50,56,62,68,69,70,71,77,83,82,81,80,79,85,91,92,93,94,95,96]; 
-
+//this part grabs all the cells, changes the background color and adds text, just to test out jquery
   var $boxes = $(".cell");
   $boxes.html("Hello, little boxes");
   $boxes.css("background-color","white");
 
-  // $(gameSpots).each(function (index, value) {
-  //   $('.cell').each(function () {
-  //     $('.cell').attr("data-spot-id", index +1);
-  //   });  
-  // });
-    
-$(gameSpots).each(function (id, index) {
-  $('.cell').each(function () {
-    $('.cell').attr("data-spot-id", id +1);
-  });
-});
-
-  //use .each on spots array
+//Here I use .each on gameSpots array
   $.each(gameSpots, function (index, value) {
   console.log(index + ":" + value);
   });
 
-  //use .each on cell class
+//Here I use .each on cell class
   $('.cell').each(function (index,value) {
   console.log(index);
   });
 
+// Here I need to apply data-spot-id w/numbers 1-45 to corresponding cells with the matching index, like gameSpots[44]= cell[95]. Something is working but not what I want
+$.each(gameSpots, function (i, id) {
+    var cell = $('#' + id); //there is something misssing here
+    $('.cell').attr("data-spot-id", i +1);
+  });
+  
 
- 
-
- });
+});
 });
